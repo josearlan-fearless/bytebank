d3b17ae46beb9e40ffd52c8776d3bccbd186bbfc
Jose Arlan <josearlan.fearless@gmail.com>
2024-02-07 16:26:05 -0300

    Mais sobre variáveis

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,12 @@
 fun main() {
     println("Hello World!")
     val titular = "Alex"
+    val numeroConta = 1000
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
+
     println("Titular $titular")
+    println("Número da conta $numeroConta")
+    println("Saldo da conta $saldo")
 }
