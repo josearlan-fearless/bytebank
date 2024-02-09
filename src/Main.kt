21410876671424086a5e5cd47bf5a18021b66eee
Jose Arlan <josearlan.fearless@gmail.com>
2024-02-09 16:28:47 -0300

    Cópia e referência

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,52 @@
 fun main() {
-    println("Hello World!")
+    println("Bem vindo ao Bytebank")
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1000
+    contaAlex.saldo = 200.0
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+//
+//    println(contaAlex.titular)
+//    println(contaAlex.numero)
+//    println(contaAlex.saldo)
+//
+//    println(contaFran.titular)
+//    println(contaFran.numero)
+//    println(contaFran.titular)
+
+    var numeroX = 10
+    var numeroY = numeroX
+    numeroY++
+
+    println("numeroX $numeroX")
+    println("numeroY $numeroY")
+
+    val contaJoao = Conta()
+    contaJoao.titular = "João"
+//    var contaMaria = contaJoao //Aqui tem atribuição por referência
+    var contaMaria = Conta()  //Aqui é criada uma nova instância de objeto
+    contaMaria.titular = "Maria"
+    contaJoao.titular = "João"
+
+    println("titular conta joao: ${contaJoao.titular}")
+    println("titular conta maria: ${contaMaria.titular}")
+
+    println(contaJoao)
+    println(contaMaria)
+
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+fun testaLacos() {
     var i = 0
     while (i < 5){
         val titular = "Alex $i"
