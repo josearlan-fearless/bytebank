7ddb1cfb1f4563a76121e826dc45848db3fe0c1f
Jose Arlan <josearlan.fearless@gmail.com>
2024-02-12 16:58:09 -0300

    Curso 2: Criando classes para cada funcionário

--- src/Funcionario.kt
+++ src/Funcionario.kt
@@ -1,21 +1,11 @@
 class Funcionario(
     val nome: String,
     val cpf: String,
-    var salario: Double,
-    val tipo: Int  // 0 funcionario, 1 Gerente,  2 Diretor
+    var salario: Double
 ) {
 
     fun bonificacao(): Double {
-        when (tipo) {
-            0 -> {
-                return salario * 0.1
-            }
-            1 -> {
-                return salario * 0.2
-            }
-            else -> {
-                return salario * 0.3
-            }
-        }
+        return salario * 0.1
     }
+
 }
--- src/Main.kt
+++ src/Main.kt
@@ -4,13 +4,28 @@ fun main() {
     val alex = Funcionario(
         nome = "Alex",
         cpf = "111.111.111-11",
-        salario = 1000.0,
-        tipo = 2
+        salario = 1000.0
     )
 
     println("Nome ${alex.nome}")
     println("CPF ${alex.cpf}")
     println("Salario ${alex.salario}")
     println("bonificação ${alex.bonificacao()}")
-    println("tipo ${alex.tipo}")
+
+    val fran = Gerente(
+        nome = "Fran",
+        cpf = "222.222.222-22",
+        salario = 2000.0,
+        senha = 1234
+    )
+
+    println("Nome ${fran.nome}")
+    println("CPF ${fran.cpf}")
+    println("Salario ${fran.salario}")
+    println("bonificação ${fran.bonificacao()}")
+    if (fran.autentica(1234)) {
+        println("autenticou com sucesso!")
+    } else {
+        println("Falha na autenticação!")
+    }
 }
