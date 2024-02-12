8830953533d335d9b088082e4f74603c8958884c
Jose Arlan <josearlan.fearless@gmail.com>
2024-02-12 16:00:51 -0300

    Curso 2: Criando uma nova Classe

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,14 @@
 fun main() {
+    println("Bem vindo ao Bytebank")
 
+    val alex = Funcionario(
+        nome = "Alex",
+        cpf = "111.111.111-11",
+        salario = 1000.0
+    )
+
+    println("Nome ${alex.nome}")
+    println("CPF ${alex.cpf}")
+    println("Salario ${alex.salario}")
+    println("bonificação ${alex.bonificacao()}")
 }
