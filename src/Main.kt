3bb1e3a7756a2548fe7a5962ee6368490858d2a4
Jose Arlan <josearlan.fearless@gmail.com>
2024-02-12 18:26:00 -0300

    Herdando de funcionário

--- src/Main.kt
+++ src/Main.kt
@@ -28,4 +28,26 @@ fun main() {
     } else {
         println("Falha na autenticação!")
     }
+
+    val gui = Diretor(
+        nome = "Gui",
+        cpf = "333.333.333-33",
+        salario = 4000.0,
+        senha = 4000,
+        plr = 200.0
+    )
+
+    println("Nome ${gui.nome}")
+    println("CPF ${gui.cpf}")
+    println("Salario ${gui.salario}")
+    println("bonificação ${gui.bonificacao()}")
+    println("plr ${gui.plr}")
+    if (gui.autentica(4000)) {
+        println("autenticou com sucesso!")
+    } else {
+        println("Falha na autenticação!")
+    }
+
+
+
 }
