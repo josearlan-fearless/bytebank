6921098d7af8db5bd810368ebe9e983fe4e319a4
Jose Arlan <josearlan.fearless@gmail.com>
2024-02-15 15:36:34 -0300

    Curso 2: Utilizando interfaces

--- src/Cliente.kt
+++ src/Cliente.kt
@@ -2,5 +2,10 @@ class Cliente(
     val nome: String,
     val cpf: String,
     var senha: Int
-) {
+) : Autenticavel {
+    override fun autentica(senha: Int): Boolean {
+        if (this.senha == senha)
+            return true
+        return false
+    }
 }
