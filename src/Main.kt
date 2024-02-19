db51deff0d30dd01858833c7aceadcb53c70c7c0
Jose Arlan <josearlan.fearless@gmail.com>
2024-02-19 17:01:40 -0300

    Curso 3: Técnicas de imports e apelidos

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,11 @@
-import teste.testaComportamentosConta
+import modelo.Cliente
+import novomodelo.Cliente as NovoCliente
+import java.lang.String as StringJava
 
 fun main() {
-    testaComportamentosConta()
+    val palavra: kotlin.String = ""
+    val palavraJava: StringJava = StringJava("")
+
+    val cliente = Cliente("Alex", "111.111.111-11", 1234)
+    val clienteNovo = NovoCliente()
 }
