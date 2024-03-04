a8e6319a1c5e660d1d21646692c0559247b909b6
Jose Arlan <josearlan.fearless@gmail.com>
2024-03-04 18:40:34 -0300

    Curso 3: Utilizando o Any no código

--- src/br/com/alura/bytebank/Main.kt
+++ src/br/com/alura/bytebank/Main.kt
@@ -1,38 +1,24 @@
 package br.com.alura.bytebank
 import br.com.alura.bytebank.modelo.*
+import br.com.alura.bytebank.teste.testaFuncionarios
 import br.com.alura.bytebank.teste.testaObjects
+import java.util.Objects
 
 
 fun main() {
-    println()
-    println(1)
-    println(1.0)
-    println(true)
-    println("teste")
-
-    imprime()
-    imprime(1)
-    imprime(1.0)
-
     val endereco = Endereco()
 
-    println(endereco)
-    println(endereco)
-//    println(Cliente())
+    val objeto : Any = Any()
+    imprime(objeto)
+
+    imprime(1)
+    imprime(1.0)
+    val teste: Any = imprime(endereco)
+    println(teste )
+    testaFuncionarios()
 }
 
-fun imprime() {
-
-}
-
-fun imprime(valor: Int) {
-
-}
-
-fun imprime(valor: Double) {
-
-}
-
-fun imprime(endereco: Endereco) {
-
+fun imprime(valor : Any) : Any {
+    println(valor)
+    return valor
 }
